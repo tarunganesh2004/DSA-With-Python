594391281326870cb07c7b36e3889c9e1f081226
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-06-13 18:05:59 +0530

    feat: add dynamic programming tabulation and space optimized solutions for partition equal subset sum problem

--- DP/Variations Of 0-1 Knapsack/partitionEqualSubsetSum.py
+++ DP/Variations Of 0-1 Knapsack/partitionEqualSubsetSum.py
@@ -54,3 +54,32 @@ def memoization_2d(arr, cur_ele, target, dp):
 target=sum(nums)//2
 dp=[[-1]*(target+1) for _ in range(len(nums)+1)]
 print(memoization_2d(nums, 0, target, dp)) # O(n*target)
+
+# dynamic programming tabulation
+def subset_sumdp(arr,target):
+    # dp[i][j] will be True if a subset with sum j can be formed using the first i elements
+    n=len(arr)
+    dp=[[False]*(target+1) for _ in range(n+1)] # O(n*target)
+    for i in range(n+1):
+        dp[i][0] = True  # A sum of 0 can always be formed with an empty subset
+    for i in range(1, n + 1):
+        for j in range(1, target + 1):
+            if arr[i - 1] <= j:
+                dp[i][j] = dp[i - 1][j] or dp[i - 1][j - arr[i - 1]]
+            else:
+                dp[i][j] = dp[i - 1][j]
+    return dp[n][target]
+print(subset_sumdp(nums, sum(nums)//2)) # O(n*target)
+
+# Space optimized dynamic programming tabulation
+def subset_sum_space_optimized(arr, target):
+    n = len(arr)
+    dp = [False] * (target + 1)
+    dp[0] = True  # A sum of 0 can always be formed with an empty subset
+
+    for i in range(n):
+        for j in range(target, arr[i] - 1, -1):
+            dp[j] = dp[j] or dp[j - arr[i]]
+    
+    return dp[target]
+print(subset_sum_space_optimized(nums, sum(nums)//2))  # O(n*target) with O(target) space
