21ae42cc121d51e911816343151ad1c16455ba99
tarunganesh2004 <tarun.21bce8974@vitapstudent.ac.in>
2025-07-02 12:03:57 +0530

    Refactor longest substring with at most K distinct characters implementation and add example usage

--- Sliding Window/longestSubstringWithAtmostKcharacters.py
+++ Sliding Window/longestSubstringWithAtmostKcharacters.py
@@ -1,1 +1,22 @@
 # Longets Substring with atmost K distinct characters
+
+s="eceba"
+k=2
+
+def longestSubstringWithAtmostKCharacters(s, k):
+    n=len(s)
+    count = {}
+    left, max_len = 0, 0
+    for right in range(n):
+        count[s[right]] = count.get(s[right], 0) + 1
+        
+        while len(count) > k:
+            count[s[left]] -= 1
+            if count[s[left]] == 0:
+                del count[s[left]]
+            left += 1
+            
+        max_len = max(max_len, right - left + 1)
+    return max_len
+
+print(longestSubstringWithAtmostKCharacters(s, k))  
